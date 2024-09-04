ff7de755c8be79f006aeaeda685d914b4a59ae99
dhalfks <miran5093@gmail.com>
2024-09-04 12:35:55 +0900

    day01

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Test from "./components/Test";
+import Test2 from "./components/Test2";
+import Test3 from "./components/Test3";
+
+function App() {
+  return (
+    <div className="App">
+      <div>Hello React!!!</div>
+      <Test />
+      <Test />
+      <Test />
+      <Test2 />
+      <Test2 />
+      <Test2 />
+      <Test3 />
+    </div>
+  );
+}
+
+export default App;
